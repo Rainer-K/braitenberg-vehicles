Fixed units, 2D and 3D, added view of light

src/entities.py
@@ -7,10 +7,45 @@ import utils
 class Entity:
 
     def __init__(self):
-        # 2D position = (x,y)
-        self.pos = np.array([0, 0])
+        pass
 
-class Agent(Entity):
+
+class Entity2D(Entity):
+
+    def __init__(self):
+        # 2D position = (x,y)
+        self._pos = np.array([0, 0])
+
+    def get_pos2D(self):
+        return self._pos
+
+    def set_pos2D(self, pos2D):
+        assert len(pos2D) == 2
+        self._pos = pos2D
+
+
+class Entity3D(Entity):
+
+    def __init__(self):
+        # 3D poisiton = (x,y,z)
+        self._pos = np.array([0, 0, 0])
+
+    def get_pos2D(self):
+        return self._pos[0:2]
+
+    def get_pos3D(self):
+        return self._pos
+
+    def set_pos2D(self, pos2D):
+        assert len(pos2D) == 2
+        self._pos[0:2] = pos2D
+
+    def set_pos3D(self, pos3D):
+        assert len(pos3D) == 3
+        self._pos = pos3D
+
+
+class Agent(Entity2D):
 
     def __init__(self):
         super(Agent, self).__init__()
@@ -18,28 +53,10 @@ class Agent(Entity):
         self.ori = 0.
 
 
-class LightSource(Entity):
+class LightSource(Entity3D):
 
     def __init__(self, luminosity):
         super(LightSource, self).__init__()
         self.luminosity = luminosity
-
-    def brightness(self, pos):
-        # shading by Fresnel effect
-        # normal: (0, 0, 1) (z points up)
-        # intensity = N * L, normal N, L vector to light (both normalized)
-        normal = np.array([0., 0., 1.])
-        light_3D_pos = np.array(list(self.pos) + [utils.config.lightsource_height])
-        point_3D_pos = np.array(list(pos) + [0.])
-
-        to_light = light_3D_pos - point_3D_pos
-        # normalize
-        to_light /= np.linalg.norm(to_light)
-
-        brightness = self.luminosity * np.dot(normal, to_light)
-
-        return brightness
-
-
 
 

src/environment.py
@@ -16,16 +16,32 @@ class Environment:
         self.agent = entities.Agent()
         self.lightSources = [entities.LightSource(config.lightsource_luminosity)]
 
-        self.agent.pos = self.random_pos()
+        self.agent.set_pos2D(self.random_pos())
 
         for light in self.lightSources:
-            #light.set_pos(self.size[0] / 4, self.size[1]/2)
-            light.pos = self.random_pos()
+            light.set_pos3D(np.concatenate((self.random_pos(),
+                                            np.array([config.lightsource_height]))))
+            print("Lightsource position: {}".format(light.get_pos3D()))
 
 
     def brightness_at_pos(self, pos):
-        return sum([light.brightness(pos) for light in self.lightSources])
+        return sum([self.brightness_of_lightsource(pos, light)
+                    for light in self.lightSources])
 
     def random_pos(self):
         return np.array([random.uniform(0, self.size[0]),
                          random.uniform(0, self.size[1])])
+
+    def brightness_of_lightsource(self, pos, lightsource):
+        # shading by Fresnel effect
+        # normal: (0, 0, 1) (z points up)
+        # intensity = N * L, normal N, L vector to light (both normalized)
+        normal = np.array([0., 0., 1.])
+
+        to_light = lightsource.get_pos3D() - np.array(list(pos) + [0.])
+        # normalize
+        to_light /= np.linalg.norm(to_light)
+
+        brightness = lightsource.luminosity * np.dot(normal, to_light)
+
+        return brightness

src/main.py
@@ -1,4 +1,5 @@
 
+import numpy as np
 import pygame as pg
 
 from environment import Environment
@@ -13,9 +14,14 @@ screen = pg.display.set_mode(config.screen_size, 0, 32)
 environment = Environment(config.environment_size)
 view = View(environment)
 
-print("Lightsource pos: ({}, {})".format(*environment.lightSources[0].pos))
+print("Lightsource pos: ({}, {})".format(*environment.lightSources[0].get_pos2D()))
 
 screen.blit(view.background, (0,0))
+screen.blit(view.light_sources, (0, 0))
+
+def update_title(mouse_pos):
+    pos_meters = utils.pixels_to_meters(np.array(mouse_pos))
+    pg.display.set_caption("Cursor position (m): {}".format(pos_meters))
 
 while True:
 
@@ -23,6 +29,8 @@ while True:
         if event.type == pg.QUIT:
             pg.quit()
             exit()
+        if event.type == pg.MOUSEMOTION:
+            update_title(event.pos)
         if event.type == pg.MOUSEBUTTONDOWN:
             scale = event.pos[0] / float(config.screen_width)
 

src/utils.py
@@ -22,14 +22,15 @@ class Config:
         self.environment_size = np.array([self.environment_width,
                                           self.environment_height])
 
-        self.lightsource_luminosity = config["lightsource_luminosity"]
-        self.lightsource_height = config["lightsource_height"] # in m
+        self.lightsource_luminosity = config["light_luminosity"]
+        self.lightsource_height = config["light_height"] # in m
 
         k_diffuse_dict = config["k_diffuse"]
         self.k_diffuse = np.array([k_diffuse_dict["r"],
                                    k_diffuse_dict["g"],
                                    k_diffuse_dict["b"]])
 
+        self.light_symbol = config["light_symbol"]
 
 config = Config()
 

src/view.py
@@ -1,5 +1,6 @@
 
-import pygame
+import numpy as np
+import pygame as pg
 
 import utils
 from utils import config
@@ -11,8 +12,15 @@ class View:
 
         self.environment = environment
 
-        self.background = pygame.Surface(self.screen_size)
+        self.background = pg.Surface(self.screen_size)
         self.render_background()
+
+        self._light_source_symbol = View.create_light_source_symbol()
+
+        self.light_sources = pg.Surface(self.screen_size).convert_alpha()
+        self.render_light_sources()
+
+
 
     def render_background(self):
         self.background.lock()
@@ -28,3 +36,40 @@ class View:
                 self.background.set_at((x,y), pixel_color)
 
         self.background.unlock()
+
+    def render_light_sources(self):
+        self.light_sources.fill((0, 0, 0, 0))
+        for light in self.environment.lightSources:
+            symbol_size = self._light_source_symbol.get_size()
+            pos = utils.meters_to_pixels(light.get_pos2D())
+            blit_pos = (pos[0] - symbol_size[0]/2,
+                        pos[1] - symbol_size[1]/2)
+
+        self.light_sources.blit(self._light_source_symbol, blit_pos)
+
+
+    @classmethod
+    def create_light_source_symbol(cls):
+        # draws a 4 pointed star
+        inset = 0.3
+        points = np.array([
+            (0,1), (inset,inset), (1,0), (inset, -inset),
+            (0,-1), (-inset,-inset), (-1,0), (-inset,inset)
+        ])
+        # scale and shift
+        size = config.light_symbol["size"]
+        points *= size/2
+        points += size/2
+
+        surface = pg.Surface((size, size)).convert_alpha()
+        surface.fill((0, 0, 0, 0))
+        color = pg.Color(config.light_symbol["color"])
+        pg.draw.polygon(surface, color, points)
+        #pg.draw.aalines(surface, color, True, points)
+
+        return surface
+
+
+
+
+