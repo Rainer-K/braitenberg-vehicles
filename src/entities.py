a9fad0b820464206e80a497f436b76fabe7f760b
Rainer Kartmann <rainer.kartmann@student.kit.edu>
2017-06-27 19:23:14 +0200

    Implemented View

--- src/entities.py
+++ src/entities.py
@@ -10,10 +10,6 @@ class Entity:
         # 2D position = (x,y)
         self.pos = np.array([0, 0])
 
-    def set_pos(self, x, y):
-        self.pos[0] = x
-        self.pos[1] = y
-
 class Agent(Entity):
 
     def __init__(self):
@@ -33,16 +29,16 @@ class LightSource(Entity):
         # normal: (0, 0, 1) (z points up)
         # intensity = N * L, normal N, L vector to light (both normalized)
         normal = np.array([0., 0., 1.])
-        light_3D_pos = np.array(list(map(utils.pixels_to_meters, self.pos)) + [utils.config.lightsource_height])
-        point_3D_pos = np.array(list(map(utils.pixels_to_meters, pos)) + [0.])
+        light_3D_pos = np.array(list(self.pos) + [utils.config.lightsource_height])
+        point_3D_pos = np.array(list(pos) + [0.])
 
         to_light = light_3D_pos - point_3D_pos
         # normalize
         to_light /= np.linalg.norm(to_light)
 
-        intensity = self.luminosity * np.dot(normal, to_light)
+        brightness = self.luminosity * np.dot(normal, to_light)
 
-        return intensity
+        return brightness
 
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 
+import numpy as np
 import yaml
-
 
 class Config:
     def __init__(self):
@@ -8,7 +8,7 @@ class Config:
             try:
                 config = yaml.safe_load(configfile)
             except yaml.YAMLError as exc:
-                print(exc)
+                raise exc
 
         self.screen_width = config["screen_width"]
         self.screen_height = config["screen_height"]
@@ -17,8 +17,18 @@ class Config:
 
         self.pixel_to_meter_ratio = self.screen_extent / max(*self.screen_size)
 
+        self.environment_width = self.screen_width * self.pixel_to_meter_ratio
+        self.environment_height = self.screen_height * self.pixel_to_meter_ratio
+        self.environment_size = np.array([self.environment_width,
+                                          self.environment_height])
+
         self.lightsource_luminosity = config["lightsource_luminosity"]
         self.lightsource_height = config["lightsource_height"] # in m
+
+        k_diffuse_dict = config["k_diffuse"]
+        self.k_diffuse = np.array([k_diffuse_dict["r"],
+                                   k_diffuse_dict["g"],
+                                   k_diffuse_dict["b"]])
 
 
 config = Config()
